e74f54ba39be200833078ee11e3acfa9c66693f3
Arnav Verma <109273538+Arnav7107@users.noreply.github.com>
2023-02-28 22:49:31 +0530

    Update app.js

--- app.js
+++ app.js
@@ -19,7 +19,6 @@ app.post("/",function(req,res)
     const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&appid=" + apiKey + "&units=" + unit;
     https.get(url, function(response)
     {
-        
         console.log(response.statusCode);
 
         response.on("data",function(data)
